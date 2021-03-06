ebe63a4566d870a0f2e757fe89ab1ae6bc45a463
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-03-06 15:40:01 -0300

    Add project files.

---
+++ src/DevIO.AppMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevIO.AppMVC.MvcApplication" Language="C#" %>
---
+++ src/DevIO.AppMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevIO.AppMVC.Startup))]
+namespace DevIO.AppMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
