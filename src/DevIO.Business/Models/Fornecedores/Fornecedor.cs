3ec519dc967d166c962f0997330082b47c49d96d
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-03-06 16:10:09 -0300

    Criando Entidades de négocio

---
+++ src/DevIO.Business/Models/Fornecedores/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using DevIO.Business.Core.Models;
+using DevIO.Business.Models.Produtos;
+using System.Collections.Generic;
+
+namespace DevIO.Business.Models.Fornecedores
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /*EF Relations*/
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
