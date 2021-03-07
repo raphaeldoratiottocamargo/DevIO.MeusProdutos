bc0aadc1a4b77f439f337d70b27e14dc7b57b240
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-03-07 12:53:17 -0300

    Mapeando o banco de dados: repositórios especializados

---
+++ src/DevIO.Infra/Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using DevIO.Business.Models.Fornecedores;
+
+namespace DevIO.Infra.Data.Repository
+{
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    {
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            //return await Db.Enderecos.AsNoTracking().FirstOrDefaultAsync(e => e.Id == fornecedorId);//id do endereço é PK e FK
+
+            return await ObterPorId(fornecedorId);
+        }
+    }
+}
---
+++ src/DevIO.Infra/Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,25 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DevIO.Infra.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(f => f.Endereco)
+                .FirstOrDefaultAsync(f => f.Id == id);
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(f => f.Endereco)
+                .Include(f => f.Produtos)
+                .FirstOrDefaultAsync(f => f.Id == id);
+        }
+    }
+}
