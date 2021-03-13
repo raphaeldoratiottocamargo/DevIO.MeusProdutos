fc9da3eca1c0f020f9398de16a4fb96553ef4e6d
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-03-13 17:11:10 -0300

    Implementado  ProdutoService

---
+++ src/DevIO.Business/Models/Produtos/Services/IProdutoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Produtos.Services
+{
+    public interface IProdutoService: IDisposable
+    {
+
+        Task Adicionar(Produto produto);
+
+        Task Atualizar(Produto produto);
+
+        Task Remover(Guid id);
+
+
+    }
+}
---
+++ src/DevIO.Business/Models/Produtos/Services/ProdutoService.cs
@@ -0,0 +1,44 @@
+﻿using DevIO.Business.Core.Services;
+using DevIO.Business.Models.Produtos.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Produtos.Services
+{
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+
+            await _produtoRepository.Adicionar(produto);
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+
+            await _produtoRepository.Atualizar(produto);
+        }
+
+        public async Task Remover(Guid id)
+        {
+            await _produtoRepository.Remover(id);
+        }
+
+        public void Dispose()
+        {
+            _produtoRepository?.Dispose();
+        }
+    }
+}
