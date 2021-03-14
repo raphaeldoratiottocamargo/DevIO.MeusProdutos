aa7e56058f23a59c30baba75cfacade8af6dc4a4
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-03-14 11:15:10 -0300

    Iniciando a camada de apresentação com a modelagem das ViewModels

---
+++ src/DevIO.AppMVC/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DevIO.AppMVC.ViewModels
+{
+    public class FornecedorViewModel
+    {
+        public FornecedorViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        [DisplayName("Tipo")]
+        public TipoFornecedor TipoFornecedor { get; set; }
+
+        public EnderecoViewModel Endereco { get; set; }
+
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+
+        public IEnumerable<ProdutoViewModel> Produtos { get; set; }
+
+
+    }
+}
